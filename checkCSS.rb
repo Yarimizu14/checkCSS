f05eae4d46749919f3dad1b27b06594949edf799
Yarimizu14 <yarimizu.kensei14@gmail.com>
2013-07-21 01:39:26 +0900

    Outputs all parameters

--- checkCSS.rb
+++ checkCSS.rb
@@ -10,6 +10,11 @@ def print_selector(file)
   css_tree = css.__send__(:build_tree)
   css_tree.select {|n| n.is_a?(Sass::Tree::RuleNode) }.each do |rule|
     pp rule.parsed_rules.members
+    pp rule.parsed_rules.members.class
+    pp rule.parsed_rules.members.size
+    rule.parsed_rules.members.each do |selector|
+        pp selector
+    end
   end
 end
 
